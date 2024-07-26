e9159b6953621f21775f1fbcb103cd04b09f9d03
Marvelousy20 <afosmarvel@gmail.com>
2024-07-26 14:25:53 +0100

    fixing bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} max-w-md mx-auto overflow-hidden`}>
+      <body className={`${inter.className} max-w-md mx-auto overflow-x-hidden`}>
         {children}
       </body>
     </html>
